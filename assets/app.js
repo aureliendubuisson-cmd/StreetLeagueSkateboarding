ade49ed72dbc417cc00c27ea7fc298366cc31b5a
Emmanuel Barat <emmanuel.barat@les-tilleuls.coop>
2026-03-30 15:29:06 +0200

    feat: improve style

--- assets/app.js
+++ assets/app.js
@@ -5,5 +5,7 @@
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
